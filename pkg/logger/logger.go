55b7f7ab19d1b5f71ab9b26c1dc998791f9bd638
vindosVp <vadiminmail@gmail.com>
2024-10-22 13:50:46 +0300

    logger levels fix

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -16,14 +16,17 @@ const (
 
 func SetupLogger(env string, serviceName string) zerolog.Logger {
 	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
-	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
-	zl := zerolog.New(output)
+	var zl zerolog.Logger
 	switch env {
 	case envDev:
+		output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
+		zl = zerolog.New(output)
 		zl.Level(zerolog.DebugLevel)
 	case envProd:
+		zl = zerolog.New(os.Stdout)
 		zl.Level(zerolog.InfoLevel)
 	case envTest:
+		zl = zerolog.New(os.Stdout)
 		zl.Level(zerolog.Disabled)
 	}
 	return zl.With().Timestamp().Str("service", serviceName).Str("env", env).Logger()
